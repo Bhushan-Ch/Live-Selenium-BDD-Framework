c6b1ddd05cd98a6f6ee64a136a586fde6f1d9a6c
Bhushan-Ch <bhushanchougule8525@gmail.com>
2026-05-31 21:15:58 +0530

    modified files

--- src/main/java/utils/DriverFactory.java
+++ src/main/java/utils/DriverFactory.java
@@ -1,49 +1,87 @@
 package utils;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.safari.SafariDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
-	
-	public static WebDriver driver;
-	
-	public static WebDriver initializeBrowser(String browserName) {
-		if (browserName.equalsIgnoreCase("chrome")) {
-			WebDriverManager.chromedriver().setup();
-			driver = new ChromeDriver();
-			
-		}else if(browserName.equalsIgnoreCase("firefox")) {
-			WebDriverManager.firefoxdriver().setup();
-			driver = new FirefoxDriver();
-			
-		}else if(browserName.equalsIgnoreCase("edge")) {
-			WebDriverManager.edgedriver().setup();
-			driver = new EdgeDriver();
-			
-		}else if(browserName.equalsIgnoreCase("safari")) {
-			WebDriverManager.safaridriver().setup();
-			driver = new SafariDriver();
-			
-		}
-		
-		driver.manage().deleteAllCookies();
-		driver.manage().window().maximize();
-		//driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(CommonUtils.PAGE_LOAD_TIME));
-		//driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(CommonUtils.IMPLICIT_WAIT_TIME));
-		
-		return driver;
-		
-	}
-	
-	public static WebDriver getDriver() {
-		
-		return driver;
-	
 
-}
+    private static WebDriver driver;
+
+    public static WebDriver initializeBrowser(String browserName) {
+
+        if (browserName.equalsIgnoreCase("chrome")) {
+
+            WebDriverManager.chromedriver().setup();
+
+            ChromeOptions options = new ChromeOptions();
+
+            // Jenkins compatible
+            options.addArguments("--headless=new");
+            options.addArguments("--disable-gpu");
+            options.addArguments("--window-size=1920,1080");
+            options.addArguments("--remote-allow-origins=*");
+            options.addArguments("--disable-notifications");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            driver = new ChromeDriver(options);
+
+        } else if (browserName.equalsIgnoreCase("firefox")) {
+
+            WebDriverManager.firefoxdriver().setup();
+
+            FirefoxOptions options = new FirefoxOptions();
+
+            options.addArguments("--headless");
+
+            driver = new FirefoxDriver(options);
+
+        } else if (browserName.equalsIgnoreCase("edge")) {
+
+            WebDriverManager.edgedriver().setup();
+
+            EdgeOptions options = new EdgeOptions();
+
+            options.addArguments("--headless=new");
+
+            driver = new EdgeDriver(options);
+
+        } else {
+
+            throw new RuntimeException(
+                    "Browser not supported : " + browserName);
+        }
+
+        driver.manage().deleteAllCookies();
+
+        driver.manage().timeouts()
+                .implicitlyWait(Duration.ofSeconds(10));
+
+        driver.manage().timeouts()
+                .pageLoadTimeout(Duration.ofSeconds(60));
+
+        return driver;
+    }
+
+    public static WebDriver getDriver() {
+
+        return driver;
+    }
+
+    public static void quitBrowser() {
+
+        if (driver != null) {
+
+            driver.quit();
+        }
+    }
 }
